a12ae2353c64c7561339ba32b42d7a564947fd1c
kaidenpemberton <kaiden_pemberton1@my.vcccd.edu>
2021-10-15 13:20:52 -0700

    ih

--- assignments/assignment2_4/assignment2_4.cpp
+++ assignments/assignment2_4/assignment2_4.cpp
@@ -11,10 +11,10 @@ int main()
     double numbers[N] = {10.0, 15.2, 21.2, 10.5, 11.3, 12.5,
     13.8, 14.7, 19.8, 20.0};
 
-   /* The address of the first element of the array is set
-    in my code as the initial value passed through the function,
-    the address of the array is also known as the first item of that array. 
-    That is since in memory, the addresses of the array follow the first value. */
+   /* The address of the first element of the array is set, 
+      in my code as the initial value passed through the function,
+      the address of the array is also known as the first item of that array. 
+      That is since in memory, the addresses of the array follow the first value. */
     printArray(numbers, N); 
 }
 
